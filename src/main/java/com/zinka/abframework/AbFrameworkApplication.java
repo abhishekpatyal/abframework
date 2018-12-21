bc95bb97d068c9bc54c334d3f5117b7913c1065b
Abhishek <abhishek.patyal@blackbuck.com>
2018-12-21 13:31:22 +0530

    first push to repo

---
+++ src/main/java/com/zinka/abframework/AbFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.zinka.abframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class AbFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbFrameworkApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/zinka/abframework/AbFrameworkApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zinka.abframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class AbFrameworkApplicationTests {
+
+    @Test public void contextLoads() {
+    }
+
+}
+
